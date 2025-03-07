ed8c14be92fd264821fb9d0ecee982b6638697d4
nhp <ricardoryn1317@gmail.com>
2025-03-08 01:10:34 +0800

    change Chinese font in ubuntu

--- config/fonts.lua
+++ config/fonts.lua
@@ -6,9 +6,13 @@ local font_family = 'JetBrainsMono Nerd Font'
 local font_size = platform.is_win and 11 or 13
 
 return {
-    font = wezterm.font({
-        family = font_family,
-        weight = 'Medium',
+    -- font = wezterm.font({
+    --     family = font_family,
+    --     weight = 'Medium',
+    -- }),
+    font = wezterm.font_with_fallback({
+        font_family,
+        { family = 'Microsoft YaHei', scale = 1 },
     }),
     font_size = font_size,
 

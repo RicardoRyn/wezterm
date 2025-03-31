961ebeb01b974dd4bd34e6c57d239557c8435db8
Ricardo_T8 <ricardoryn1317@gmail.com>
2025-03-31 15:12:13 +0800

    更改tab样式

--- events/tab-title.lua
+++ events/tab-title.lua
@@ -35,8 +35,8 @@ local nf = wezterm.nerdfonts
 
 local M = {}
 
-local GLYPH_SCIRCLE_LEFT = nf.ple_left_half_circle_thick --[[  ]]
-local GLYPH_SCIRCLE_RIGHT = nf.ple_right_half_circle_thick --[[  ]]
+local GLYPH_SCIRCLE_LEFT = nf.ple_lower_right_triangle --[[  ]]
+local GLYPH_SCIRCLE_RIGHT = nf.ple_lower_left_triangle --[[  ]]
 local GLYPH_CIRCLE = nf.fa_circle --[[  ]]
 local GLYPH_ADMIN = nf.md_shield_half_full --[[ 󰞀 ]]
 local GLYPH_LINUX = nf.cod_terminal_linux --[[  ]]
@@ -89,17 +89,17 @@ local RENDER_VARIANTS = {
 ---@type table<string, Cells.SegmentColors>
 -- stylua: ignore
 local colors = {
-    text_default = { bg = '#45475A', fg = '#1C1B19' },
-    text_hover = { bg = '#5D87A3', fg = '#1C1B19' },
-    text_active = { bg = '#74c7ec', fg = '#11111B' },
+    text_default = { bg = '#45475A', fg = '#eff1f5' },
+    text_hover = { bg = '#5D87A3', fg = '#45475A' },
+    text_active = { bg = '#eff1f5', fg = '#45475A' },
 
     unseen_output_default = { bg = '#45475A', fg = '#FFA066' },
     unseen_output_hover = { bg = '#5D87A3', fg = '#FFA066' },
-    unseen_output_active = { bg = '#74c7ec', fg = '#FFA066' },
+    unseen_output_active = { bg = '#eff1f5', fg = '#FFA066' },
 
     scircle_default = { bg = 'rgba(0, 0, 0, 0.4)', fg = '#45475A' },
     scircle_hover = { bg = 'rgba(0, 0, 0, 0.4)', fg = '#5D87A3' },
-    scircle_active = { bg = 'rgba(0, 0, 0, 0.4)', fg = '#74C7EC' }
+    scircle_active = { bg = 'rgba(0, 0, 0, 0.4)', fg = '#eff1f5' }
 }
 
 ---
